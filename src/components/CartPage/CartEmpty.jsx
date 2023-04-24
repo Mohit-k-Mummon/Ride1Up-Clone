b53906f975441107f1ba22f4155a013b9e99231c
Mohit Mummon <mxkumar@protonmail.com>
2023-04-23 18:49:43 -0700

    Convert CartEmpty styles to css module

--- src/components/CartPage/CartEmpty.jsx
+++ src/components/CartPage/CartEmpty.jsx
@@ -4,17 +4,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ride1upLogo from '../../assets/Cart/ride-up-bg-logo.png';
 
+import styles from './CartEmpty.module.css';
+
 const CartEmpty = () => {
 	return (
 		<>
-			<div className='cart-empty-state'>
-				<div className='cart-empty-state__icon'></div>
+			<div className={styles['cart-empty']}>
+				<div className={styles.icon}></div>
 				<p>Your cart is currently empty.</p>
 			</div>
-			<div className='bg-logo'>
+			<div className={styles['bg-logo']}>
 				<img src={ride1upLogo} alt='' />
 			</div>
-			<Link to={'/'} className='cart-return-home-link'>
+			<Link to={'/'} className={styles['return-link']}>
 				Return to Home
 			</Link>
 		</>
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './page-styles/CartPage.css';
-
 import Cart from '../components/CartPage/Cart';
 
 const CartPage = () => {
